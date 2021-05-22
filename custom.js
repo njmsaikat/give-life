2c1f25531f92481dbee0aeab0496993206de6ee3
Saikat Roy <njmsaikat@gmail.com>
2021-05-22 21:00:05 +0600

    button changed

---
+++ custom.js
@@ -0,0 +1,8 @@
+function btnClick() {
+    var x = document.getElementById("other-amount");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
